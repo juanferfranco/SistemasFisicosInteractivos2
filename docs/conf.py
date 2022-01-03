e080e04b33b8b9f1ca5d379f2491886030e49bb8
Juan Franco <juanferfranco@gmail.com>
2022-01-03 10:42:26 -0500

    Init 2022-10

--- docs/conf.py
+++ docs/conf.py
@@ -20,11 +20,11 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Sistemas Físicos Interactivos 2'
-copyright = '2021, Juan Franco'
+copyright = '2022, Juan Franco'
 author = 'Juan Franco'
 
 # The short X.Y version
-version = '0.9.0'
+version = '0.9.1'
 # The full version, including alpha/beta/rc tags
 release = 'beta'
 
